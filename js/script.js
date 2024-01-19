7349a9b7696cf73d1ef2e01e1a9cf7c6b9b554de
Walace Rodrigues <walaceclash10@gmail.com>
2024-01-19 13:59:54 -0300

    modo noturno e animação

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// ================= typing animation
+var typed = new Typed(".typing", {
+   strings:["Desenvolvedor de Sistemas","Desenvolvedor Front-end","Desenvolvedor Back-end","Desenvolvedor de Sistemas"],typeSpeed:100,
+   backSpeed:60,
+   loop:false
+})
--- js/style-switcher.js
+++ js/style-switcher.js
@@ -13,3 +13,32 @@ window.addEventListener("scroll", () => {
 })
 
 // ====================== theme colors
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color){
+   alternateStyles.forEach((style) => {
+      if(color === style.getAttribute("title")){
+         style.removeAttribute("disabled");
+      }
+      else {
+         style.setAttribute("disabled", "true");
+      }
+   })
+}
+
+// ====================== theme dia e noite
+const dayNight = document.querySelector(".day-night")
+
+dayNight.addEventListener("click", () => {
+   dayNight.querySelector("i").classList.toggle("fa-sun")
+   dayNight.querySelector("i").classList.toggle("fa-moon")
+   document.body.classList.toggle("dark")
+})
+
+window.addEventListener("load", () => {
+   if(document.body.classList.contains("dark")){
+      dayNight.querySelector("i").classList.add("fa-sun")
+   }
+   else {
+      dayNight.querySelector("i").classList.add("fa-moon")
+   }
+})
